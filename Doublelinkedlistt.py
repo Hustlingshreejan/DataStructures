0fd3a9a39c2c5ed8de91326461a2840dbaad6252
Shreejan Shrestha <Hustlingshreejan@gmail.com>
2020-11-26 19:35:04 +0545

    V.2 added method to delete the element by index

--- Doublelinkedlistt.py
+++ Doublelinkedlistt.py
@@ -38,19 +38,56 @@ class DoubleLinedList:
             itr3 = itr3.next
         return count
 
-    
-dll= DoubleLinedList()
+    def remove_element_by_index(self, index):
+        if self.head is None:
+            print("Empty")
+            return
+        if index < 0 | index > self.get_len():
+            raise Exception("Invalid index")
+
+        if index == 0:
+            self.head = self.head.next
+            self.head.prev = None
+            return
+
+        if index == self.get_len():
+            itr4 = self.head
+            count = 0
+            while itr4:
+                if count == index - 1:
+                    itr4.next = None
+                    return
+
+        itr4 = self.head
+        count = 0
+        while itr4:
+            if count == index - 1:
+                itr4.next = itr4.next.next
+                itr4.next.next.prev = itr4.next
+                return
+            itr4 = itr4.next
+            count += 1
+
+
+dll = DoubleLinedList()
 dll.add_beginning(2)
 dll.add_beginning(3)
 dll.add_beginning(5)
-dll.show_Doublelinkedlist()
+# dll.show_Doublelinkedlist()
 dll.add_end(4)
-dll.show_Doublelinkedlist()
+# dll.show_Doublelinkedlist()
 dll.add_end(4444)
-dll.show_Doublelinkedlist()
+# dll.show_Doublelinkedlist()
 dll.add_beginning(333)
 dll.show_Doublelinkedlist()
 print(dll.get_len())
+dll.remove_element_by_index(2)
+dll.show_Doublelinkedlist()
+print(dll.get_len())
+
+# print("***")
+# dll.remove_element_by_index(4)
+# dll.show_Doublelinkedlist()
 
 
 
