0ca48e9d20c1b9811a8d0ebe72409874186a6ced
Shreejan Shrestha <Hustlingshreejan@gmail.com>
2020-11-26 14:59:21 +0545

    V.1 for Singlelinkedlist

---
+++ FoodorderThreadimplementation.py
@@ -0,0 +1,48 @@
+import time
+import threading
+import queueimplementation
+
+class Foodorder:
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def sqaure(listt):
+#     for num in listt:
+#         time.sleep(2)
+#         print(f'square of {num} is {num*num}\n')
+#
+#
+# def cube(listt):
+#     for num in listt:
+#         time.sleep(2)
+#         print(f'cube of {num} is {num*num*num}\n')
+#
+#
+# a = [2,3,4,5,6]
+# t1 = threading.Thread(target=sqaure, args=(a,))
+# t2 = threading.Thread(target=cube, args=(a,))
+#
+# t1.start()
+# t2.start()
+#
+# t1.join()
+# t2.join()
---
+++ Linkedlistimplementation.py
@@ -0,0 +1,87 @@
+class Node:
+    def __init__(self, element=None, node=None):
+        self.element = element
+        self.node = node
+
+
+class Linkedlist:
+    def __init__(self):
+        self.head = None
+
+    def add_beginning(self, element):
+        new_element = Node(element, None)
+        new_element.next = self.head
+        self.head = new_element
+
+    def add_end(self, element):
+        new_element = Node(element, None)
+        if self.head is None:
+            new_element = self.head
+            new_element.next = None
+
+        iterr = self.head
+        while iterr.next is not None:
+            iterr = iterr.next
+        iterr.next = new_element
+        new_element.next = None
+
+    def get_length(self):
+        count = 0
+        itr3 = self.head
+        while itr3:
+            count += 1
+            itr3 = itr3.next
+        return count
+
+    def remove_element(self, index):
+        if self.head is None:
+            print("Empty")
+
+        if index < 0 | index > self.get_length():
+            raise Exception("Invalid Index")
+
+        if index == 0:
+            self.head = self.head.next
+            return
+
+        count = 0
+        itr4 = self.head
+        while itr4:
+            if count == index-1:
+                itr4.next = itr4.next.next
+            itr4 = itr4.next
+            count += 1
+
+    def add_from_index(self, index, element):
+        if index < 0 | index > self.get_length():
+            raise Exception("Invalid Index")
+        if index == 0:
+            self.add_beginning(element)
+
+        count = 0
+        itr5 = self.head
+        while itr5:
+            if count == index-1:
+                new_element = Node(element, itr5.next)
+                new_element.next = itr5.next
+                itr5.next = new_element
+
+            itr5 = itr5.next
+            count += 1
+
+    def show_linkedlist(self):
+        if self.head is None:
+            print("Empty")
+        iter2 = self.head
+        Llstr = ''
+        while iter2:
+            Llstr += str(iter2.element) + '--->'
+            iter2 = iter2.next
+        print(Llstr)
+
+
+if __name__ == '__main__':
+    print("Name of all methods:\n1:add_beginning.\n2:add_end.\n3:get_length.\n4:remove_element.\n5:add_from_index.\n6:show_linkedlist")
+    ll = Linkedlist()
+
+
