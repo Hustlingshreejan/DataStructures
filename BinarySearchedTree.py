627986d5576aba62136940682e8bbbe743bac393
Shreejan Shrestha <Hustlingshreejan@gmail.com>
2020-12-02 12:33:09 +0545

    V.2. Added preorder traversor, min and max method

--- BinarySearchedTree.py
+++ BinarySearchedTree.py
@@ -1,60 +1,74 @@
-class BinarySearchedTree:
+class BinarySearchTree:
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
+        self.parent = None
 
     def add_child(self, data):
         if data == self.data:
-            print("Number already exists. Binary Searched tree can only have unique element.")
+            # print("Binary Searched tree cannot have a duplicate value. Duplicate one has been removed")
             return
 
         if data < self.data:
             if self.left:
                 self.left.add_child(data)
             else:
-                self.left = BinarySearchedTree(data)
+                self.left = BinarySearchTree(data)
         else:
             if self.right:
                 self.right.add_child(data)
             else:
-                self.right = BinarySearchedTree(data)
+                self.right = BinarySearchTree(data)
 
-    def inorder_traversal(self):
-        elements = []
+    def get_min(self):
         if self.left:
-            elements += self.left.inorder_traversal()
+            return self.left.get_min()
+        else:
+            return "The minimum value for this tree is", self.data
 
-        elements.append(self.data)
+    def get_max(self):
+        if self.right:
+            a = self.right.get_max()
+            return a
+        return self.data
+
+    def inorder_travers(self):
+        lis = []
+        if self.left:
+            lis += self.left.inorder_travers()
+
+        lis.append(self.data)
 
         if self.right:
-            elements += self.right.inorder_traversal()
-        # elements.append(self.data)
-        return elements
+            lis += self.right.inorder_travers()
 
-    def search_element(self, element):
-        if element == self.data:
-            return True
-        if element < self.data:
-            if self.left:
-                return self.left.search_element(element)
-            else:
-                return False
-        else:
-            if self.right:
-                return self.right.search_element(element)
-            else:
-                return False
-def BinaryTree(lis):
-    root = BinarySearchedTree(lis[0])
-    for i in range(1, len(lis)):
-        root.add_child(lis[i])
+        return lis
+
+    def preorder_travers(self):
+        emp = []
+        emp.append(self.data)
+        if self.left:
+            # emp.append(self.left)
+            emp += self.left.preorder_travers()
+        if self.right:
+            # emp.append(self.right)
+            emp += self.right.preorder_travers()
+        return emp
+
+
+def print_binary_tree(dataset):
+    # print(len(dataset))
+    root = BinarySearchTree(dataset[0])
+    for i in range(1, len(dataset)):
+        root.add_child(dataset[i])
     return root
 
 
-
 if __name__ == '__main__':
-    data = [100,70,101,50,80,81,75,25,60,30,10]
-    a = BinaryTree(data)
-    print(a.inorder_traversal())
-    print(a.search_element(666))
+    all_data = [55, 10, 12, 33, 55, 56, 89, 12, 8, 44, 555, 110]
+    shree = print_binary_tree(all_data)
+    print(shree.inorder_travers())
+    print(shree.get_min())
+    print(shree.get_max())
+    print(shree.preorder_travers())
