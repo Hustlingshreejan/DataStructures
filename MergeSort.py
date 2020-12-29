3f5992efae883558823b6efd137f1c3976cab821
Shree_jan <hustlingshreejan@gmail.com>
2020-12-29 14:06:54 +0545

    V.2. sorting array in inline. Extra Space consumption solved

--- MergeSort.py
+++ MergeSort.py
@@ -1,40 +1,41 @@
-def merge_two_sorted_list(a,b):
-    sorted_list = []
+def merge_two_sorted_list(a,b, arr):
     len_a = len(a)
     len_b = len(b)
 
-    i = j = 0
+    i = j = k = 0
     while i < len_a and j < len_b:
         if a[i] <= b[j]:
-            sorted_list.append(a[i])
+            arr[k] = a[i]
             i += 1
         else:
-            sorted_list.append(b[j])
+            arr[k] = b[j]
             j += 1
-
+        k += 1
     while i < len_a:
-        sorted_list.append(a[i])
+        arr[k] = a[i]
         i += 1
+        k += 1
 
     while j < len_b:
-        sorted_list.append(b[j])
+        arr[k] = b[j]
         j += 1
-    return sorted_list
+        k += 1
 
 
 def merge(arr):
     if len(arr) <= 1:
-        return arr
+        return
     midpoint = len(arr) // 2
     left_side = arr[:midpoint]
     right_side = arr[midpoint:]
 
-    left = merge(left_side)
-    right = merge(right_side)
+    merge(left_side)
+    merge(right_side)
 
-    return merge_two_sorted_list(left, right)
+    merge_two_sorted_list(left_side, right_side,arr)
 
 
 if __name__ == '__main__':
     a = [11,2,111,1,3,56,346,32,1111]
-    print(merge(a))
+    merge(a)
+    print(a)
